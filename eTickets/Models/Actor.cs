c28ef7afab49b6e50e57ef0fc13257c7be702c68
claudia <claudia_curca@yahoo.com>
2023-07-12 18:16:58 +0300

    Add ProducerView

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -7,10 +7,12 @@ namespace eTickets.Models
 		[Key]
 		public int Id { get; set; }
 
-		[Display(Name = "ProfilePictureURL")]
+		[Display(Name = "Profile Picture")]
 		public string ProfilePictureURL { get; set; }
-		[Display(Name = "FullName")]
+
+		[Display(Name = "Full Name")]
 		public string FullName { get; set; }
+
 		[Display(Name = "Bio")]
 		public string Bio { get; set; }
 
