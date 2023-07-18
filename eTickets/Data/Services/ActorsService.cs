bebd94cc2b9901c0e17a430617b6f3767b20036f
claudia <claudia_curca@yahoo.com>
2023-07-18 17:10:34 +0300

    add actorsServices

---
+++ eTickets/Data/Services/ActorsService.cs
@@ -0,0 +1,33 @@
+﻿using eTickets.Models;
+
+namespace eTickets.Data.Services
+{
+    public class ActorsService : IActorsService
+    {
+
+        public void Add(Actor actor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Actor> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Actor GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Actor Update(int id, Actor newActor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
