5a741d73326e3ca8e59e99e80380b1d93b189ce1
claudia <claudia_curca@yahoo.com>
2023-07-27 17:46:07 +0300

    modify actor service

---
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -0,0 +1,47 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data.Base
+{
+	public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
+	{
+		private readonly AppDBContext _context;
+
+		public EntityBaseRepository(AppDBContext context)
+		{
+			_context = context;
+		}
+
+		public async Task AddAsync(T entity)
+		{
+			await _context.Set<T>().AddAsync(entity);
+			await _context.SaveChangesAsync();
+		}
+
+		public async Task DeleteAsync(int id)
+		{
+			var result = GetByIdAsync(id);
+			_context.Set<T>().Remove(result.Result);
+			await _context.SaveChangesAsync();
+		}
+
+		public async Task<IEnumerable<T>> GetAllAsync()
+		{
+			var result = await _context.Set<T>().ToListAsync();
+			return result;
+		}
+
+		public async Task<T> GetByIdAsync(int id)
+		{
+			var result = await _context.Set<T>().FirstOrDefaultAsync(x => x.Id == id);
+			return result;
+		}
+
+		public async Task<T> UpdateAsync(int id, T entiry)
+		{
+			_context.Set<T>().Update(entiry);
+			await _context.SaveChangesAsync();
+			return entiry;
+		}
+	}
+}
